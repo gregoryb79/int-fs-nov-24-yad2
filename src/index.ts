00461706e8bca5596788170fb2ca6725652aa1a3
dolevoper <omerdolev90@gmail.com>
2025-03-16 17:42:44 +0200

    setup server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello, world!!");
+import { createServer } from "http";
+
+const server = createServer((req, res) => {
+    console.log(req.method, req.url);
+
+    res.end();
+});
+
+server.listen(8090, () => console.log("Server listening on port 8090"));
