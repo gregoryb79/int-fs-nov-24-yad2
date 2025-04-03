948426f1332732d27faf01ca5f1a820c92fe7949
dolevoper <omerdolev90@gmail.com>
2025-04-03 21:30:26 +0300

    get port from env

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,14 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const server = createServer(app);
+const port = process.env.PORT ?? 8090;
 
 async function init() {
     await mongoose.connect(process.env.CONNECTION_STRING!, {
         dbName: "yad2",
     });
 
-    server.listen(8090, () => console.log("Server listening on port 8090"));
+    server.listen(port, () => console.log(`Server listening on port ${port}`));
 }
 
 init();
